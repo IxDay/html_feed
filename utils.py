0839fe7f9a0117a1d5c0bc058da85d31a72b2539
IxDay <maxime.vidori@gmail.com>
2012-11-12 22:09:13 +0100

    yaml corrections, parse link function is done

--- utils.py
+++ utils.py
@@ -5,22 +5,46 @@ __author__ = 'mvidori'
 
 
 def initialize(document):
-    links_struct = {}
-
-
-    def parse_links(struct,document):
-        if document.has_key("link") :
-            for elt in document['link']:
-                Link(elt)
-
-        else :
-            for key in document:
-                if document[key].has_key("link") :
-                    struct[key] = []
+    def parse_links(struct, document):
+        if isinstance(document, list):
+            for elt in document:
+                if isinstance(elt,dict):
+                    link = Link(elt['entitled'])
+                    link.courses = elt['courses']
                 else:
-                    struct[key] = {}
-                parse_links(struct[key],document[key])
+                    link = Link(elt)
+                struct += [link]
+        else:
+            for key, value in document.items():
+                struct[key] = type(value)()
+                parse_links(struct[key], value)
 
-    parse_links(links_struct, document["links"])
-    print(links_struct)
+
+    def parse_parsing(struct, document):
+        for key, value in document.items():
+            if key in struct :
+                parse_parsing(struct[key],value)
+            else:
+                parse_catchers(struct,key,value)
+
+    def get_links(struct):
+        if isinstance(struct,Link):
+            return
+
+    def parse_catchers(struct,tag,attr):
+        pass
+
+
+
+
+
+
+
+
+    links_struct = {}
+    parse_links(links_struct, document['links'])
+    parse_parsing(links_struct, document['parsing'])
+
+
+#    print(links_struct)
 
