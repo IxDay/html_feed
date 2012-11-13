59ea061ba80e207f5b20b0192aabd9550a496ab5
maxime vidori <maxime.vidori@gmail.com>
2012-11-13 14:11:45 +0100

    parsing done

--- pages.py
+++
@@ -1,9 +0,0 @@
-__author__ = 'mvidori'
-
-class Pages:
-    def __init__(self,groups,links):
-        self.groups = groups
-        self.links = links
-
-    def __str__(self):
-        print()
--- utils.py
+++ utils.py
@@ -5,10 +5,23 @@ __author__ = 'mvidori'
 
 
 def initialize(document):
+    def get_links(struct):
+        if isinstance(struct, Link):
+            return [struct]
+        if not len(struct):
+            return []
+        if isinstance(struct, dict):
+            struct = dict(struct)
+            return get_links(struct.popitem()[1]) + get_links(struct)
+        if isinstance(struct, list):
+            struct = list(struct)
+            return get_links(struct.pop()) + get_links(struct)
+
+
     def parse_links(struct, document):
         if isinstance(document, list):
             for elt in document:
-                if isinstance(elt,dict):
+                if isinstance(elt, dict):
                     link = Link(elt['entitled'])
                     link.courses = elt['courses']
                 else:
@@ -20,30 +33,26 @@ def initialize(document):
                 parse_links(struct[key], value)
 
 
-    def parse_parsing(struct, document):
+    def parse_parsing_next(struct, document, is_parsing):
         for key, value in document.items():
-            if key in struct :
-                parse_parsing(struct[key],value)
+            if key in struct:
+                parse_parsing_next(struct[key], value,is_parsing)
             else:
-                parse_catchers(struct,key,value)
-
-    def get_links(struct):
-        if isinstance(struct,Link):
-            return
-
-    def parse_catchers(struct,tag,attr):
-        pass
-
-
-
-
+                links = get_links(struct)
+                for link in links:
+                    if is_parsing:
+                        link.catchers[key] = value
+                    else :
+                        link.next[key] = value
 
 
 
 
     links_struct = {}
     parse_links(links_struct, document['links'])
-    parse_parsing(links_struct, document['parsing'])
+    parse_parsing_next(links_struct, document['parsing'],True)
+    parse_parsing_next(links_struct, document['next'],False)
+    print('toto')
 
 
 #    print(links_struct)
